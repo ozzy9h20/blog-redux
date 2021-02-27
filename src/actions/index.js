989f122ee951364056e2c74553ef6a2c48ccab8c
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-02-27 22:21:46 +0700

    [BAD CODE] Consume API in redux
    Error: Actions must be plain objects. Use custom middleware for async actions.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,11 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = async () => {
+  // BAD APPROACH!
+  const response = await jsonPlaceholder.get('/posts');
+
   return {
     type: 'FETCH_POST',
+    payload: response
   };
 };
